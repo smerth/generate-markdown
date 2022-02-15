84964839725f373ebd5496124e18ae5c2b07f47d
smerth <stephen.merth@gmail.com>
2022-02-15 13:42:02 -0800

    fix(markdown): remove img element from generated posts

--- src/utilities/generate-text.js
+++ src/utilities/generate-text.js
@@ -22,15 +22,15 @@ export const longText = json2md([
   { p: lorem.generateParagraphs(2) },
   { h2: lorem.generateSentences(1) },
   { p: lorem.generateParagraphs(1) },
-  {
-    img: [
-      {
-        title: "Some image",
-        source:
-          "https://d2r55xnwy6nx47.cloudfront.net/uploads/2022/02/SCALING_NETS_2880x1620_Lede.svg",
-      },
-    ],
-  },
+  // {
+  //   img: [
+  //     {
+  //       title: "Some image",
+  //       source:
+  //         "https://d2r55xnwy6nx47.cloudfront.net/uploads/2022/02/SCALING_NETS_2880x1620_Lede.svg",
+  //     },
+  //   ],
+  // },
   { p: lorem.generateParagraphs(1) },
 ]);
 
